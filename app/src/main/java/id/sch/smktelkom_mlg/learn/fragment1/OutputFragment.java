1b4fb1146dca91e4919a5610000d61b9dc90fdd9
Yessy Permatasari <yessypermatasari08@gmail.com>
2017-01-22 10:03:33 -0800

    Fragment Independent View and Process.

--- app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/OutputFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/OutputFragment.java
@@ -1,18 +1,17 @@
 package id.sch.smktelkom_mlg.learn.fragment1;
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OutputFragment extends Fragment {
-
+    TextView tvHasil;
 
     public OutputFragment() {
         // Required empty public constructor
@@ -23,7 +22,13 @@ public class OutputFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_output, container, false);
+        View view = inflater.inflate(R.layout.fragment_output, container, false);
+        tvHasil = (TextView) view.findViewById(R.id.textViewHasil);
+        return view;
+    }
+
+    public void doProses(String nama, String umur) {
+        tvHasil.setText(nama + " (" + umur + " tahun)");
     }
 
 }
